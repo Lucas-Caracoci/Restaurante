253ee402250420907ab7821d44c6b8a10634dd97
Lucas Caracoci <127033819+Lucas-Caracoci@users.noreply.github.com>
2024-03-19 18:02:52 -0300

    Update index.jsx

--- src/components/botoes/index.jsx
+++ src/components/botoes/index.jsx
@@ -6,27 +6,27 @@ export default function Botoes({handleFiltro,botaoClicado}){
             
             <div className="container-botoes">
                 <button className={botaoClicado === "Entradas" ? "acenderBtn" : "apagarBtn"} onClick={() => handleFiltro("Entradas")} >
-                    <img className="imgBtn" src='/assets/entrada.png' alt=''/>
+                    <img className="imgBtn" src='/Assets/entrada.png' alt=''/>
                     <span>Entradas</span>
                 </button>
                 <button className={botaoClicado === "Massas" ? "acenderBtn" : "apagarBtn"}  onClick={() => handleFiltro("Massas")} >
-                    <img className="imgBtn" src='/assets/massa.png' alt=''/>
+                    <img className="imgBtn" src='/Assets/massa.png' alt=''/>
                     <span>Massas</span>
                 </button>
                 <button className={botaoClicado === "Carnes" ? "acenderBtn" : "apagarBtn"} onClick={() => handleFiltro("Carnes")} >
-                    <img className="imgBtn" src='/assets/carne.png' alt=''/>
+                    <img className="imgBtn" src='/Assets/carne.png' alt=''/>
                     <span>Carnes</span>
                 </button>
                 <button className={botaoClicado === "Bebidas" ? "acenderBtn" : "apagarBtn"} onClick={() => handleFiltro("Bebidas")} >
-                    <img className="imgBtn" src='/assets/bebidas.png' alt=''/>
+                    <img className="imgBtn" src='/Assets/bebidas.png' alt=''/>
                     <span>Bebidas</span>
                 </button>
                 <button className={botaoClicado === "Saladas" ? "acenderBtn" : "apagarBtn"} onClick={() => handleFiltro("Saladas")} >
-                    <img className="imgBtn" src='/assets/salada.png' alt=''/>
+                    <img className="imgBtn" src='/Assets/salada.png' alt=''/>
                     <span>Saladas</span>
                 </button>
                 <button className={botaoClicado === "Sobremesas" ? "acenderBtn" : "apagarBtn"}  onClick={() => handleFiltro("Sobremesas")}>
-                    <img className="imgBtn" src='/assets/sobremesa.png' alt=''/>
+                    <img className="imgBtn" src='/Assets/sobremesa.png' alt=''/>
                     <span>Sobremesas</span>
                 </button>
                 
